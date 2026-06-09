e3f977281d0ded1a1fe22efc85282d5bf2c58311
Bhuvadej Anankusri <phuvadej.anks@gmail.com>
2026-06-09 11:24:39 +0700

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ export default {
   out: "./src/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DIRECT_URL!,
   },
   verbose: true,
   strict: true,
